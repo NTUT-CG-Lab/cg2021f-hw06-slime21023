96739074fbddf8a2da0482c767b27b3eeff6cb83
slime21023 <slime21023@gmail.com>
2021-12-03 11:58:28 +0800

    finish

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ import {
     sRGBEncoding
 } from './build/three.module.js'
 
-const names = ['dolphin', 'apple']
+const names = ['dolphin', 'cat', 'deer', 'hand']
 // The iterations of pytorch3d models
 const models = [0, 5, 10, 20, 30, 40, 50, 60, 80, 100, 130, 160, 190, 220, 250, 300, 400, 500, 600, 800, 1000, 1250, 1500, 1750, 2000]
 const scene = new Scene()
@@ -26,14 +26,14 @@ const loadAssets = async (names) => {
     const material = new MeshPhongMaterial({ color: 0xAAAAAA, specular: 0x111111, shininess: 200 })
     const loadModels = n => {
         const loader = new PLYLoader()
-        const location = [`./models/${n}.ply`, ...models.map(m => `./models/${n}_${m}.ply`)]
+        const location = [`./models/${n}.ply`, ...models.map(m => `./models/${n}_model_${m}.ply`)]
         const loadItem = (item, index) => new Promise(resolve => {
             loader.load(item, geometry => {
                 geometry.computeVertexNormals()
                 const mesh = new Mesh(geometry, material)
                 mesh.name = item
                 mesh.position.set(index == 0 ? -0.5 : 0.5, -0.5, 0)
-                mesh.scale.multiplyScalar(1.5)
+                mesh.scale.multiplyScalar(1)
                 mesh.castShadow = true
                 mesh.receiveShadow = true
                 mesh.visible = false
@@ -46,9 +46,10 @@ const loadAssets = async (names) => {
     }
 
     const assets = {}
-    names.forEach(async n => {
-        const models = await loadModels(n)
-        assets[n] = models
+    const retModels = await Promise.all(names.map(async n => await loadModels(n)))
+
+    names.forEach((n, index) => {
+        assets[n] = retModels[index]
     })
     return assets
 }
@@ -60,7 +61,7 @@ const init = async (assets) => {
 
     // Camera
     const camera = new PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 15)
-    camera.position.set(3, .015, 3)
+    camera.position.set(100, .015, 100)
 
     const cameraTarget = new Vector3(0, 0.1, 0)
 
@@ -118,14 +119,12 @@ const init = async (assets) => {
     })
 
     let gui = null
-    let curModel = null
+    let curModel = []
     const changeModel = () => {
         if (gui) gui.destroy()
-        if (curModel) {
-            assets[curModel].forEach(item => { item.visible = false })
-        }
+        curModel.forEach(item => { item.visible = false })
 
-        const value = select.value
+        const value = `${select.value}`
         curModel = assets[value]
         curModel[0].visible = true
         curModel[1].visible = true
@@ -163,6 +162,7 @@ const init = async (assets) => {
 }
 
 loadAssets(names).then(async assets => {
+    // console.log(assets)
     const animate = await init(assets)
     animate()
 })
